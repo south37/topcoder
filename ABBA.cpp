091f2dea6d2888929f686662a92a469018de6d11
Nao Minami <south37777@gmail.com>
2017-06-11 13:56:56 +0900

    Use string

--- ABBA.cpp
+++ ABBA.cpp
@@ -4,67 +4,46 @@
 #include <set>
 #include <queue>
 
-typedef unsigned long long unsigned int ulong;
-
 class ABBA {
 public:
     std::string canObtain(std::string initial, std::string target) {
-        ulong i = changeToLong(initial);
-        if (fromTarget(target, initial.length()) == i) {
+        fromTarget(target, initial.length());
+        if (target == initial) {
             return "Possible";
         } else {
             return "Impossible";
         }
     }
-    ulong fromTarget(const std::string& target, int len) {
-        ulong r = changeToLong(target);
+    void fromTarget(std::string& target, int len) {
         int r_len = target.length();
         while (r_len > len) {
-            if (canRemoveA(r)) {
-                r = removeA(r);
+            if (canRemoveA(target)) {
+                removeA(target);
             } else {
-                r = removeB(r, r_len);
+                removeB(target);
             }
             r_len -= 1;
         }
-        return r;
     }
-    bool canRemoveA(ulong n) {
-        return (n & 0x01) == 0;
+    bool canRemoveA(const std::string& s) {
+        return *(--s.end()) == 'A';
     }
-    ulong removeA(ulong n) {
-        return n >> 1;
+    void removeA(std::string& s) {
+        s.erase(--s.end());
     }
-    ulong removeB(ulong n, int len) {
-        return reverseWithLen((n >> 1), len - 1);
-    }
-    ulong changeToLong(const std::string& s) {
-        ulong result = 0;
-        auto end = s.end();
-        for(auto it = s.begin(); it != end; ++it) {
-            result <<= 1;
-            if (*it == 'B') {  // If 'A', 0 is set
-                result += 1;
-            }
-        }
-        return result;
-    }
-    ulong reverseWithLen(ulong n, int len) {
-        return reverse(n) >> (sizeof(long) * CHAR_BIT - len);
-    }
-    ulong reverse(ulong n) {
-        ulong rv = 0;
-        std::size_t b = sizeof(ulong) * CHAR_BIT;
-        for (std::size_t i = 0; i < b; ++i, n >>= 1) {
-            rv = (rv << 1) | (n & 0x01);
-        }
-        return rv;
+    void removeB(std::string& s) {
+        s.erase(--s.end());
+        std::reverse(s.begin(), s.end());
     }
 };
 
 int main(int argc, char** argv) {
     ABBA a0;
     std::cout << "Possible is expected, and got " << a0.canObtain("B", "ABBA") << std::endl;
+    // std::string s("ABBA");
+    // std::cout << s << std::endl;
+    // a0.fromTarget(s, 1);
+    // std::cout << s << std::endl;
     ABBA a1;
     std::cout << "Impossible is expected, and got " << a1.canObtain("AB", "ABB") << std::endl;
     ABBA a2;
