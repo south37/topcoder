cafb4616a13b3eabecbb782abcd1f9ca62a172c7
Nao Minami <south37777@gmail.com>
2017-06-17 18:03:02 +0900

    Use reference in foreach loop

--- Aaamnrs.cpp
+++ Aaamnrs.cpp
@@ -8,7 +8,7 @@ class Aaagmnrs {
 public:
     std::vector <std::string> anagrams(std::vector <std::string> phrases) {
         std::vector < std::string > result;
-        for(auto s : phrases) {
+        for(auto &s : phrases) {
             std::string original = s;
             normalize(s);
             if (set.find(s) == set.end()) {
@@ -34,7 +34,7 @@ public:
 int main(int arc, char** argv) {
     Aaagmnrs a0;
     std::vector <std::string> phrases0 = { "Aaagmnrs", "TopCoder", "anagrams", "Drop Cote" };
-    for (auto s : a0.anagrams(phrases0)) {
+    for (auto const &s : a0.anagrams(phrases0)) {
         std::cout << s << std::endl;
     }
 }
